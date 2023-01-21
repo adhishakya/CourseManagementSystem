7c66f5d4e1cc051833df1fb36e73ab54dc0863e0
Adhish Shakya <shakyaadhish@gmail.com>
2023-01-21 20:34:10 +0545

    Teacher panel dashboard

---
+++ Frontend/TeacherPanel.java
@@ -0,0 +1,152 @@
+package courseMngmtSystem;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TeacherPanel {
+
+    public JFrame getFrmTeacherPanel() {
+        return fromTeacherPanel;
+    }
+
+    private JFrame fromTeacherPanel;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    TeacherPanel window = new TeacherPanel();
+                    window.fromTeacherPanel.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public TeacherPanel() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        fromTeacherPanel = new JFrame();
+        fromTeacherPanel.setTitle("Teacher Panel | Course Management System");
+        fromTeacherPanel.setBounds(100, 100, 634, 518);
+        fromTeacherPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JSplitPane splitPane = new JSplitPane();
+        splitPane.setDividerSize(0);
+        fromTeacherPanel.getContentPane().add(splitPane, BorderLayout.CENTER);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(128, 128, 255));
+        splitPane.setLeftComponent(panel);
+        SpringLayout sl_panel = new SpringLayout();
+        panel.setLayout(sl_panel);
+
+        JButton btnNewButton = new JButton("Dashboard");
+        btnNewButton.setIcon(new ImageIcon(
+                "D:\\College Stuffs\\Level 5\\Object-Oriented Design and Programming\\hello\\Images\\dashboard.png"));
+        sl_panel.putConstraint(SpringLayout.NORTH, btnNewButton, 33, SpringLayout.NORTH, panel);
+        sl_panel.putConstraint(SpringLayout.WEST, btnNewButton, 22, SpringLayout.WEST, panel);
+        btnNewButton.setIconTextGap(16);
+        btnNewButton.setForeground(Color.WHITE);
+        btnNewButton.setFont(new Font("Poppins", Font.BOLD, 18));
+        btnNewButton.setBorderPainted(false);
+        btnNewButton.setBorder(null);
+        btnNewButton.setBackground(new Color(128, 128, 255));
+        panel.add(btnNewButton);
+
+        JButton btnNewButton_1_1 = new JButton("Student");
+        sl_panel.putConstraint(SpringLayout.NORTH, btnNewButton_1_1, 29, SpringLayout.SOUTH, btnNewButton);
+        sl_panel.putConstraint(SpringLayout.WEST, btnNewButton_1_1, 22, SpringLayout.WEST, panel);
+        btnNewButton_1_1.setIcon(new ImageIcon(
+                "D:\\College Stuffs\\Level 5\\Object-Oriented Design and Programming\\hello\\Images\\student.png"));
+        btnNewButton_1_1.setIconTextGap(15);
+        btnNewButton_1_1.setForeground(Color.WHITE);
+        btnNewButton_1_1.setFont(new Font("Poppins", Font.BOLD, 18));
+        btnNewButton_1_1.setBorderPainted(false);
+        btnNewButton_1_1.setBorder(null);
+        btnNewButton_1_1.setBackground(new Color(128, 128, 255));
+        panel.add(btnNewButton_1_1);
+
+        JButton btnNewButton_1_1_2 = new JButton("Modules");
+        btnNewButton_1_1_2.setIcon(new ImageIcon(
+                "D:\\College Stuffs\\Level 5\\Object-Oriented Design and Programming\\hello\\Images\\books.png"));
+        sl_panel.putConstraint(SpringLayout.NORTH, btnNewButton_1_1_2, 29, SpringLayout.SOUTH, btnNewButton_1_1);
+        sl_panel.putConstraint(SpringLayout.WEST, btnNewButton_1_1_2, 0, SpringLayout.WEST, btnNewButton);
+        btnNewButton_1_1_2.setIconTextGap(14);
+        btnNewButton_1_1_2.setForeground(Color.WHITE);
+        btnNewButton_1_1_2.setFont(new Font("Poppins", Font.BOLD, 18));
+        btnNewButton_1_1_2.setBorderPainted(false);
+        btnNewButton_1_1_2.setBorder(null);
+        btnNewButton_1_1_2.setBackground(new Color(128, 128, 255));
+        panel.add(btnNewButton_1_1_2);
+
+        JButton btnNewButton_1_1_1 = new JButton("Log Out");
+        btnNewButton_1_1_1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Teacher Logging out!");
+                fromTeacherPanel.dispose();
+                Login window = new Login();
+                window.getFrmLogin().setVisible(true);
+            }
+        });
+        btnNewButton_1_1_1.setIcon(new ImageIcon(
+                "D:\\College Stuffs\\Level 5\\Object-Oriented Design and Programming\\hello\\Images\\logout.png"));
+        sl_panel.putConstraint(SpringLayout.WEST, btnNewButton_1_1_1, 0, SpringLayout.WEST, btnNewButton);
+        sl_panel.putConstraint(SpringLayout.SOUTH, btnNewButton_1_1_1, -22, SpringLayout.SOUTH, panel);
+        btnNewButton_1_1_1.setIconTextGap(16);
+        btnNewButton_1_1_1.setForeground(Color.WHITE);
+        btnNewButton_1_1_1.setFont(new Font("Poppins", Font.BOLD, 18));
+        btnNewButton_1_1_1.setBorderPainted(false);
+        btnNewButton_1_1_1.setBorder(null);
+        btnNewButton_1_1_1.setBackground(new Color(128, 128, 255));
+        panel.add(btnNewButton_1_1_1);
+
+        JSplitPane splitPane_1 = new JSplitPane();
+        splitPane_1.setDividerSize(3);
+        splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setRightComponent(splitPane_1);
+
+        JPanel panel_1 = new JPanel();
+        panel_1.setBackground(new Color(255, 255, 255));
+        splitPane_1.setLeftComponent(panel_1);
+        SpringLayout sl_panel_1 = new SpringLayout();
+        panel_1.setLayout(sl_panel_1);
+
+        JLabel lblWelcomeBackMoon = new JLabel("Welcome Back, Sir!");
+        sl_panel_1.putConstraint(SpringLayout.WEST, lblWelcomeBackMoon, 28, SpringLayout.WEST, panel_1);
+        sl_panel_1.putConstraint(SpringLayout.SOUTH, lblWelcomeBackMoon, -25, SpringLayout.SOUTH, panel_1);
+        lblWelcomeBackMoon.setFont(new Font("Poppins", Font.BOLD, 30));
+        panel_1.add(lblWelcomeBackMoon);
+
+        JPanel panel_2 = new JPanel();
+        panel_2.setBackground(new Color(255, 255, 255));
+        splitPane_1.setRightComponent(panel_2);
+        splitPane_1.setDividerLocation(100);
+        splitPane.setDividerLocation(200);
+    }
+
+}
