b64fe3fb57abe53f13906a8f30be7024ea760404
Adhish Shakya <shakyaadhish@gmail.com>
2023-02-04 19:43:46 +0545

    Added dynamic modules

--- Backend/AddTeacher.java
+++ Backend/AddTeacher.java
@@ -29,6 +29,7 @@ import javax.swing.border.BevelBorder;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.Cursor;
 import java.awt.event.ItemListener;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.awt.event.ItemEvent;
@@ -46,6 +47,60 @@ public class AddTeacher extends JDialog {
     private String assignedModule = "";
     private String isPartTimeText = "";
     private String isPartTime = "";
+    private JButton addButton;
+    private JLabel heading;
+
+    public JLabel getHeading() {
+        return heading;
+    }
+
+    public JPanel getContentPanel() {
+        return contentPanel;
+    }
+
+    public JTextField getTeacherFullNameTextField() {
+        return teacherFullNameTextField;
+    }
+
+    public JTextField getTeacherPhoneTextField() {
+        return teacherPhoneTextField;
+    }
+
+    public JTextField getTeacherAddressTextField() {
+        return teacherAddressTextField;
+    }
+
+    public ButtonGroup getButtonGroup() {
+        return buttonGroup;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public String getTeacherPhone() {
+        return teacherPhone;
+    }
+
+    public String getTeacherAddress() {
+        return teacherAddress;
+    }
+
+    public String getAssignedModule() {
+        return assignedModule;
+    }
+
+    public String getIsPartTimeText() {
+        return isPartTimeText;
+    }
+
+    public String getIsPartTime() {
+        return isPartTime;
+    }
+
+    public JButton getAddButton() {
+        return addButton;
+    }
 
     /**
      * Launch the application.
@@ -64,7 +119,7 @@ public class AddTeacher extends JDialog {
      * Create the dialog.
      */
     public AddTeacher() {
-        setTitle("Add Teacher");
+        setTitle("Add Teacher | Course Management System");
         setBounds(100, 100, 521, 467);
         getContentPane().setLayout(new BorderLayout());
         contentPanel.setBackground(new Color(255, 255, 255));
@@ -73,16 +128,15 @@ public class AddTeacher extends JDialog {
         SpringLayout sl_contentPanel = new SpringLayout();
         contentPanel.setLayout(sl_contentPanel);
 
-        JLabel lblNewLabel = new JLabel("Add New Teacher");
-        sl_contentPanel.putConstraint(SpringLayout.NORTH, lblNewLabel, 10, SpringLayout.NORTH, contentPanel);
-        sl_contentPanel.putConstraint(SpringLayout.SOUTH, lblNewLabel, 48, SpringLayout.NORTH, contentPanel);
-        sl_contentPanel.putConstraint(SpringLayout.EAST, lblNewLabel, -289, SpringLayout.EAST, contentPanel);
-        lblNewLabel.setFont(new Font("Poppins", Font.BOLD, 20));
-        contentPanel.add(lblNewLabel);
+        heading = new JLabel("Add New Teacher");
+        sl_contentPanel.putConstraint(SpringLayout.NORTH, heading, 10, SpringLayout.NORTH, contentPanel);
+        sl_contentPanel.putConstraint(SpringLayout.SOUTH, heading, 48, SpringLayout.NORTH, contentPanel);
+        sl_contentPanel.putConstraint(SpringLayout.EAST, heading, -289, SpringLayout.EAST, contentPanel);
+        heading.setFont(new Font("Poppins", Font.BOLD, 20));
+        contentPanel.add(heading);
 
         teacherFullNameTextField = new JTextField();
-        sl_contentPanel.putConstraint(SpringLayout.NORTH, teacherFullNameTextField, 14, SpringLayout.SOUTH,
-                lblNewLabel);
+        sl_contentPanel.putConstraint(SpringLayout.NORTH, teacherFullNameTextField, 14, SpringLayout.SOUTH, heading);
         teacherFullNameTextField.setCaretColor(new Color(128, 128, 255));
         teacherFullNameTextField.setSelectedTextColor(new Color(255, 255, 255));
         teacherFullNameTextField.setSelectionColor(new Color(128, 128, 255));
@@ -97,7 +151,7 @@ public class AddTeacher extends JDialog {
 
         JLabel lblNewLabel_1_1 = new JLabel("Phone No :");
         sl_contentPanel.putConstraint(SpringLayout.WEST, lblNewLabel_1_1, 26, SpringLayout.WEST, contentPanel);
-        sl_contentPanel.putConstraint(SpringLayout.WEST, lblNewLabel, 0, SpringLayout.WEST, lblNewLabel_1_1);
+        sl_contentPanel.putConstraint(SpringLayout.WEST, heading, 0, SpringLayout.WEST, lblNewLabel_1_1);
         lblNewLabel_1_1.setFont(new Font("Poppins", Font.PLAIN, 16));
         contentPanel.add(lblNewLabel_1_1);
 
@@ -177,58 +231,60 @@ public class AddTeacher extends JDialog {
         contentPanel.add(rdbtnParttime);
 
         JLabel lblNewLabel_1_1_2 = new JLabel("Full Name :");
-        sl_contentPanel.putConstraint(SpringLayout.NORTH, lblNewLabel_1_1_2, 18, SpringLayout.SOUTH, lblNewLabel);
+        sl_contentPanel.putConstraint(SpringLayout.NORTH, lblNewLabel_1_1_2, 18, SpringLayout.SOUTH, heading);
         sl_contentPanel.putConstraint(SpringLayout.WEST, lblNewLabel_1_1_2, 26, SpringLayout.WEST, contentPanel);
         sl_contentPanel.putConstraint(SpringLayout.NORTH, lblNewLabel_1_1, 35, SpringLayout.SOUTH, lblNewLabel_1_1_2);
         lblNewLabel_1_1_2.setFont(new Font("Poppins", Font.PLAIN, 16));
         contentPanel.add(lblNewLabel_1_1_2);
 
-        JButton btnNewButton = new JButton("Add");
-        btnNewButton.addActionListener(new ActionListener() {
+        addButton = new JButton("Add");
+        addButton.setActionCommand("Add");
+        addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                teacherName = teacherFullNameTextField.getText().trim();
-                teacherPhone = teacherPhoneTextField.getText().trim();
-                teacherAddress = teacherAddressTextField.getText().trim();
-                for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
-                    AbstractButton button = buttons.nextElement();
-                    if (button.isSelected()) {
-                        isPartTimeText = button.getText();
-                        if (isPartTimeText.equalsIgnoreCase("Part-Time")) {
-                            isPartTime = "1";
-                        } else {
-                            isPartTime = "0";
+                if (e.getActionCommand().equals("Add")) {
+                    teacherName = teacherFullNameTextField.getText().trim();
+                    teacherPhone = teacherPhoneTextField.getText().trim();
+                    teacherAddress = teacherAddressTextField.getText().trim();
+                    for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+                        AbstractButton button = buttons.nextElement();
+                        if (button.isSelected()) {
+                            isPartTimeText = button.getText();
+                            if (isPartTimeText.equalsIgnoreCase("Part-Time")) {
+                                isPartTime = "1";
+                            } else {
+                                isPartTime = "0";
+                            }
                         }
-                        System.out.println(isPartTime);
                     }
-                }
-                Statement statement = DatabaseConnection.getStatement();
+                    Statement statement = DatabaseConnection.getStatement();
 
-                String insertQuery = "INSERT INTO `teacherdetails` (`Id`, `teacherName`, `teacherPhoneNo`, `teacherAddress`, `assignedModule`, `isPartTime`) "
-                        + "VALUES (NULL, '" + teacherName + "', '" + teacherPhone + "', '" + teacherAddress + "', '"
-                        + assignedModule + "', '" + isPartTime + "');";
-                try {
-                    int insertSuccess = statement.executeUpdate(insertQuery);
-                    if (insertSuccess == 1) {
-                        JOptionPane.showMessageDialog(contentPanel, "Added Teacher data successfully!");
-                        AdminPanel.showDataFromDatabase();
-                        dispose();
+                    String insertQuery = "INSERT INTO `teacherdetails` (`Id`, `teacherName`, `teacherPhoneNo`, `teacherAddress`, `assignedModule`, `isPartTime`) "
+                            + "VALUES (NULL, '" + teacherName + "', '" + teacherPhone + "', '" + teacherAddress + "', '"
+                            + assignedModule + "', '" + isPartTime + "');";
+                    try {
+                        int insertSuccess = statement.executeUpdate(insertQuery);
+                        if (insertSuccess == 1) {
+                            JOptionPane.showMessageDialog(contentPanel, "Added Teacher data successfully!");
+                            AdminPanel.showTeacherDataFromDatabase();
+                            dispose();
+                        }
+                    } catch (SQLException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
                     }
-                } catch (SQLException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
                 }
 
             }
         });
-        btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        btnNewButton.setForeground(new Color(255, 255, 255));
-        btnNewButton.setBorder(null);
-        btnNewButton.setBackground(new Color(128, 128, 255));
-        sl_contentPanel.putConstraint(SpringLayout.WEST, btnNewButton, 280, SpringLayout.WEST, contentPanel);
-        sl_contentPanel.putConstraint(SpringLayout.SOUTH, btnNewButton, -15, SpringLayout.SOUTH, contentPanel);
-        btnNewButton.setFont(new Font("Poppins", Font.BOLD, 14));
-        sl_contentPanel.putConstraint(SpringLayout.NORTH, btnNewButton, 21, SpringLayout.SOUTH, rdbtnParttime);
-        contentPanel.add(btnNewButton);
+        addButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addButton.setForeground(new Color(255, 255, 255));
+        addButton.setBorder(null);
+        addButton.setBackground(new Color(128, 128, 255));
+        sl_contentPanel.putConstraint(SpringLayout.WEST, addButton, 280, SpringLayout.WEST, contentPanel);
+        sl_contentPanel.putConstraint(SpringLayout.SOUTH, addButton, -15, SpringLayout.SOUTH, contentPanel);
+        addButton.setFont(new Font("Poppins", Font.BOLD, 14));
+        sl_contentPanel.putConstraint(SpringLayout.NORTH, addButton, 21, SpringLayout.SOUTH, rdbtnParttime);
+        contentPanel.add(addButton);
 
         JButton btnNewButton_1 = new JButton("Cancel");
         btnNewButton_1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -243,7 +299,7 @@ public class AddTeacher extends JDialog {
                 dispose();
             }
         });
-        sl_contentPanel.putConstraint(SpringLayout.EAST, btnNewButton, -26, SpringLayout.WEST, btnNewButton_1);
+        sl_contentPanel.putConstraint(SpringLayout.EAST, addButton, -26, SpringLayout.WEST, btnNewButton_1);
         sl_contentPanel.putConstraint(SpringLayout.WEST, btnNewButton_1, -89, SpringLayout.EAST,
                 teacherFullNameTextField);
         btnNewButton_1.setFont(new Font("Poppins", Font.BOLD, 14));
@@ -256,11 +312,28 @@ public class AddTeacher extends JDialog {
             public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == 1) {
                     assignedModule = (String) e.getItem();
-                    System.out.println(e.getItem());
                 }
             }
         });
-        comboBox.setModel(new DefaultComboBoxModel(new String[] { "Select Module", "OODP", "AI", "NMC" }));
+
+        comboBox.setModel(new DefaultComboBoxModel(new String[] { "Select Module" }));
+        String fetchModulesQuery = "SELECT moduleName FROM `moduledetails`";
+        String[] modulesArray = new String[30];
+        int i = 0;
+        try {
+            Statement statement = DatabaseConnection.getStatement();
+            ResultSet resultSet = statement.executeQuery(fetchModulesQuery);
+
+            while (resultSet.next()) {
+                modulesArray[i] = resultSet.getString("ModuleName");
+                comboBox.addItem(modulesArray[i]);
+                i++;
+            }
+
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+
         sl_contentPanel.putConstraint(SpringLayout.NORTH, comboBox, 0, SpringLayout.NORTH, lblNewLabel_1_1_1_1);
         sl_contentPanel.putConstraint(SpringLayout.WEST, comboBox, 0, SpringLayout.WEST, teacherFullNameTextField);
         sl_contentPanel.putConstraint(SpringLayout.SOUTH, comboBox, 31, SpringLayout.NORTH, lblNewLabel_1_1_1_1);
@@ -271,5 +344,6 @@ public class AddTeacher extends JDialog {
                 new Color(255, 255, 255), new Color(255, 255, 255)));
         comboBox.setBackground(Color.WHITE);
         contentPanel.add(comboBox);
+
     }
 }
